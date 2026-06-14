e148164070ab967528a902ee6e327d12c0fcbb92
Haya Ahmed <haya.ahm.08@gmail.com>
2026-06-14 12:04:07 +0300

    added navbar

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,3 +1,7 @@
+import { Button } from "@/components/Button";
+import { Menu, X } from "lucide-react";
+import { useState } from "react";
+
 const navLinks = [
   { href: "#about", label: "About" },
   { href: "#projects", label: "Projects" },
@@ -6,6 +10,8 @@ const navLinks = [
 ];
 
 export const Navbar = () => {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
   return (
     <header className="fixed top-0 left-0 right-0 bg-transparent py-5">
       <nav className="mx-10 px-6 flex items-center justify-between">
@@ -15,8 +21,10 @@ export const Navbar = () => {
         >
           PM<span className="text-primary">.</span>
         </a>
+
+        {/* Desktop Menu */}
         <div className="glass rounded-full">
-          <div className="flex items-center gap-4 py-2 px-4">
+          <div className="hidden md:flex items-center gap-4 py-2 px-4">
             {navLinks.map((link, index) => (
               <a
                 href={link.href}
@@ -29,10 +37,34 @@ export const Navbar = () => {
           </div>
         </div>
 
-        <div>
-          <Button />
+        {/* contact btn */}
+        <div className="hidden md:block">
+          <Button children="Contact Me" size="sm" />
         </div>
+
+        {/* mobile menu btn */}
+        <button className="md:hidden p-2 text-foreground cursor-pointer" onClick={() => setMobileMenuOpen(!mobileMenuOpen)}>
+          {mobileMenuOpen ? <X /> : <Menu />}
+        </button>
       </nav>
+
+      {/* mobile menu */}
+      {mobileMenuOpen && (
+        <div className="md:hidden glass-strong animate-fade-in">
+          <div className="flex flex-col mx-auto gap-4 py-2 px-4">
+            {navLinks.map((link, index) => (
+              <a
+                href={link.href}
+                key={index}
+                className="text-lg text-muted-foreground hover:text-foreground py-2"
+              >
+                {link.label}
+              </a>
+            ))}
+            <Button children="Contact Me" />
+          </div>
+        </div>
+      )}
     </header>
   );
 };
